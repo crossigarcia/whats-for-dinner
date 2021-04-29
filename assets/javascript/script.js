2d3c4c9efea4f9d62c3a25d28d07fc1c7a2ec121
nicopetelo <nicopetelo@gmail.com>
2021-04-28 20:35:58 -0700

    two WORKING buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,6 +8,7 @@
 // });
 
 function runEdamam(){
+    console.log("runEdamam is running");
     const edamamUrl = "https://api.edamam.com/search?q=";
     const app_id = "&app_id=16faf740";
     const appKEY = "&app_key=7e13ce2d929e6839de8e33e08b528146";
@@ -20,10 +21,21 @@ function runEdamam(){
     }).then(function(response){
         console.log("AJAX Resopnse: ", response);
 
-        let hitsArray = response.hits;
-        let i = 0;
-        for(i; i < hitsArray.length; i++){
-            console.log("hits array ", hitsArray[i]);
-        }
+        // let hitsArray = response.hits;
+        // let i = 0;
+        // for(i; i < hitsArray.length; i++){
+        //     console.log("hits array ", hitsArray[i]);
+        // }
     })
 }
+
+var getMealDB = function () {
+    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian').then(function(response) {
+    response.json().then(function(data) {
+    console.log(data);
+  });
+});
+}
+
+$("#edamam-btn").on("click", runEdamam);
+$("#mealdb-btn").on("click", getMealDB);
