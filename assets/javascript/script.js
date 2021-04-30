c9503023e5bc87493b22ebb157104d149db9c8ed
crosgar <cecilia.rossi.garcia@gmail.com>
2021-04-29 19:16:19 -0700

    retrieve and display recipe name, image and ingredients, started work on next button function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,8 @@
 $('#keyword-btn').on('click', function() {
+    $('#recipe-name').html('');
+    $('#recipe-ingredients').html('');
+    $('#recipe-img').html('');
+
     let keyword = $('#keyword').val().trim();
 
     runEdamam(keyword);
@@ -10,45 +14,38 @@ function runEdamam(keyword) {
     fetch(apiUrl)
     .then(res => res.json())
     .then(data => {
-        let recipeName = $('<h2>').addClass('title').text(data.hits[0].recipe.label);
+        //let recipeHits = data.hits;
+        let hitsIndex = 0;
+
+        let recipeName = $('<h2>').addClass('title').text(data.hits[hitsIndex].recipe.label);
         $('#recipe-name').append(recipeName);
 
-        // let recipeImg = $('<img>').
+        let recipeImg = $('<img>').attr('src', data.hits[hitsIndex].recipe.image);
+        $('#recipe-img').append(recipeImg);
 
-        let ingredientsList = $('<ul>').addClass('list')
-        for (let i = 0; i < data.hits[0].recipe.ingredientLines.length; i++) {
-            let ingredientItem = $('<li>').addClass('list-item').text(data.hits[0].recipe.ingredientLines[i]);
+        let ingredientsList = $('<ul>').addClass('list');
+        for (let i = 0; i < data.hits[hitsIndex].recipe.ingredientLines.length; i++) {
+            let ingredientItem = $('<li>').addClass('list-item').text(data.hits[hitsIndex].recipe.ingredientLines[i]);
 
             ingredientsList.append(ingredientItem);
         }
-
         $('#recipe-ingredients').append(ingredientsList);
 
     });
 };
 
+$('#next-recipe-btn').on('click', nextRecipe);
 
-// function runEdamam(){
-//     console.log("runEdamam is running");
-//     const edamamUrl = "https://api.edamam.com/search?q=";
-//     const app_id = "&app_id=16faf740";
-//     const appKEY = "&app_key=7e13ce2d929e6839de8e33e08b528146";
-    
-//     let query;
+function nextRecipe(hitsIndex) {
+    let keyword = $('#keyword').val().trim();
+    $('#recipe-name').html('');
+    $('#recipe-ingredients').html('');
 
-//     $.ajax({
-//         url: edamamUrl+query+app_id+appKEY,
-//         method: "GET"
-//     }).then(function(response){
-//         console.log("AJAX Resopnse: ", response);
+    hitsIndex++;
 
-//         // let hitsArray = response.hits;
-//         // let i = 0;
-//         // for(i; i < hitsArray.length; i++){
-//         //     console.log("hits array ", hitsArray[i]);
-//         // }
-//     })
-// };
+    runEdamam(keyword);
+};
+
 
 var getMealDB = function () {
     fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian').then(function(response) {
