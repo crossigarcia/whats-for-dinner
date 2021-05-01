46b4a03002104a60cdcab430650ac3d1eeda3e18
nicopetelo <nicopetelo@gmail.com>
2021-05-01 16:22:45 -0700

    save recipe and search history present

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -37,6 +37,12 @@ function runEdamam(keyword) {
             ingredientsList.append(ingredientItem);
         }
         $('#recipe-ingredients').append(ingredientsList);
+        
+        // Saved Recipes
+
+        // trying to create an ID for edamam, which doesn't have explicit IDs
+        let recipeID = data.hits[hitsIndex].recipe.label;
+        console.log("your recipe query was = " + recipeID);
 
         let saveRecipeBtn = document.createElement("button");
         saveRecipeBtn.innerHTML = "Save This Recipe";
@@ -54,6 +60,34 @@ function runEdamam(keyword) {
 
             $(recipeEl).on('click', function reloadRecipe(){
                 // runEdamam() but for a specific recipe
+
+                // clear the current contents
+                $('#recipe-name').empty();
+                $('#recipe-img').empty();
+                $('#recipe-ingredients').empty();
+
+                // producing the result again?
+                fetch(`https://api.edamam.com/search?q=${recipeID}&app_id=f97b44b8&app_key=0ab78a3d00b18729a51ba6b69ee857d0`)
+                .then(res => res.json())
+                .then(data => {
+
+                    //let recipeHits = data.hits;
+                    let recipeName = $('<h2>').addClass('title').text(data.hits[hitsIndex].recipe.label);
+                    $('#recipe-name').append(recipeName);
+
+                    let recipeImg = $('<img>').attr('src', data.hits[hitsIndex].recipe.image);
+                    $('#recipe-img').append(recipeImg);
+
+                    let ingredientsList = $('<ul>').addClass('list');
+                    for (let i = 0; i < data.hits[hitsIndex].recipe.ingredientLines.length; i++) {
+                        let ingredientItem = $('<li>').addClass('list-item').text(data.hits[hitsIndex].recipe.ingredientLines[i]);
+
+                        ingredientsList.append(ingredientItem);
+                    }
+                    $('#recipe-ingredients').append(ingredientsList);
+                });
+
+
             });
         });
 
@@ -95,23 +129,19 @@ function searchHistory (keyword) {
     // take the current keyword search term and place it at the beginning of an array
     // searchHistoryArray.unshift(keyword);
     // console.log(searchHistoryArray);
+    var searchHistoryEl = document.querySelector("#previous-searches");
+    searchHistoryEl.classList = "enter css styling classes here"
 
-    // for (let i = 0; i < searchHistoryArray.length; i++) {
-        var searchHistoryEl = document.querySelector("#previous-searches");
-        searchHistoryEl.classList = "enter css styling classes here"
+    var searchKeywordEl = document.createElement("h5");
+    searchKeywordEl.textContent = keyword;
+    console.log(keyword);
 
-        var searchKeywordEl = document.createElement("h5");
-        searchKeywordEl.textContent = keyword;
-        console.log(keyword);
-
-        // append to the container div
-        searchHistoryEl.appendChild(searchKeywordEl);
-    // }
+    // append to the container div
+    searchHistoryEl.appendChild(searchKeywordEl);
 }
 // add a feature such that if the user hits a certain count of search queries, the button either disables or
 // the div gets a scroll box (so they can have unlimited searches)
-
-// Saved Recipes
+// scrollable div would use "overflow: scroll" in the css
 
 
 
