631fa02d4a6c37841b944472a6dcf88f89b526b2
nicopetelo <nicopetelo@gmail.com>
2021-05-02 21:29:54 -0700

    this should work

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,20 +1,12 @@
 let hitsIndex = 0;
-let recipeIndex = 0;
+
 let mealID = [];
 var listTitle = document.querySelector("#recipe-name");
 var meal_container = document.getElementById('#recipe-display');
 
-<<<<<<< HEAD
 let keyword = $('#keyword').val().trim();
 
 function clearBasicRecipeContents () {
-=======
-let mealID = [];
-var listTitle = document.querySelector("#recipe-name");
-var meal_container = document.getElementById('#recipe-steps');
-
-$('#keyword-btn').on('click', function() {
->>>>>>> 447b378eac9ed397507de20e2f012e51a843be7d
     $('#recipe-name').empty();
     $('#recipe-img').empty();
     $('#recipe-ingredients').empty();
@@ -70,67 +62,7 @@ function runEdamam(keyword) {
 
         $('#recipe-ingredients').append(ingredientsList);
 
-
-        
-        let recipeLink = $('<a>').attr('href', data.hits[hitsIndex].recipe.url).text('Click here for recipe instructions');
-
-        $('.link').append(recipeLink);
-
-        // Saved Recipes
-
-        // trying to create an ID for edamam, which doesn't have explicit IDs
-        let recipeID = data.hits[hitsIndex].recipe.label;
-        console.log("your recipe query was = " + recipeID);
-
-        let saveRecipeBtn = $('<button>').attr('id', 'save-recipe-btn').text('Save This Recipe');
-        
-        recipeName.append(saveRecipeBtn);
-
-        $(saveRecipeBtn).on('click', function saveRecipe() {
-            var savedRecipesEl = document.querySelector("#saved-recipes");
-            savedRecipesEl.classList = "enter css styling classes here";
-        
-            var recipeEl = document.createElement("button");
-            recipeEl.textContent = data.hits[hitsIndex].recipe.label;
-        
-            // append to the container div
-            savedRecipesEl.appendChild(recipeEl);
-
-            $(recipeEl).on('click', function reloadRecipe(){
-                // runEdamam() but for a specific recipe
-
-                // clear the current contents
-                $('#recipe-name').empty();
-                $('#recipe-img').empty();
-                $('#recipe-ingredients').empty();
-
-                // producing the result again?
-                fetch(`https://api.edamam.com/search?q=${recipeID}&app_id=f97b44b8&app_key=0ab78a3d00b18729a51ba6b69ee857d0`)
-                .then(res => res.json())
-                .then(data => {
-
-                    //let recipeHits = data.hits;
-                    let recipeName = $('<h2>').addClass('title').text(data.hits[hitsIndex].recipe.label);
-                    $('#recipe-name').append(recipeName);
-
-                    let recipeImg = $('<img>').attr('src', data.hits[hitsIndex].recipe.image);
-                    $('#recipe-img').append(recipeImg);
-
-                    let ingredientsList = $('<ul>').addClass('list');
-                    for (let i = 0; i < data.hits[hitsIndex].recipe.ingredientLines.length; i++) {
-                        let ingredientItem = $('<li>').addClass('list-item').text(data.hits[hitsIndex].recipe.ingredientLines[i]);
-
-                        ingredientsList.append(ingredientItem);
-                    }
-                    $('#recipe-ingredients').append(ingredientsList);
-                });
-            });
-        });
-        // end Saved Recipes
-
-        
         let recipeLink = $('<a>').attr('href', data.hits[hitsIndex].recipe.url).attr('target', '_blank').text('Click here for recipe instructions');
-
 
         $('.link').append(recipeLink);
 
@@ -217,46 +149,11 @@ function searchHistory (keyword) {
     searchHistoryEl.appendChild(searchKeywordEl);
 
     $(searchKeywordEl).on('click', function reloadRecipe(){
-<<<<<<< HEAD
         clearBasicRecipeContents();
         runEdamam(keyword);
     });
 };
 
-=======
-    // runEdamam() but for a specific recipe
-
-    // clear the current contents
-        $('#recipe-name').empty();
-        $('#recipe-img').empty();
-        $('#recipe-ingredients').empty();
-
-        // producing the result again?
-        fetch(`https://api.edamam.com/search?q=${keyword}&app_id=f97b44b8&app_key=0ab78a3d00b18729a51ba6b69ee857d0`)
-        .then(res => res.json())
-        .then(data => {
-
-            //let recipeHits = data.hits;
-            let recipeName = $('<h2>').addClass('title').text(data.hits[hitsIndex].recipe.label);
-            $('#recipe-name').append(recipeName);
-
-            let recipeImg = $('<img>').attr('src', data.hits[hitsIndex].recipe.image);
-            $('#recipe-img').append(recipeImg);
-
-            let ingredientsList = $('<ul>').addClass('list');
-            for (let i = 0; i < data.hits[hitsIndex].recipe.ingredientLines.length; i++) {
-                let ingredientItem = $('<li>').addClass('list-item').text(data.hits[hitsIndex].recipe.ingredientLines[i]);
-
-                ingredientsList.append(ingredientItem);
-            }
-            $('#recipe-ingredients').append(ingredientsList);
-    })
-    
-});}
-// add a feature such that if the user hits a certain count of search queries, the button either disables or
-// the div gets a scroll box (so they can have unlimited searches)
-// scrollable div would use "overflow: scroll" in the css
->>>>>>> 447b378eac9ed397507de20e2f012e51a843be7d
 
 //mealDB api logic:
 var getMealDB = function (category) {
@@ -265,16 +162,12 @@ var getMealDB = function (category) {
     //    console.log(data);
     //});
     //});
-    var mealID=[];
     console.log('https://www.themealdb.com/api/json/v1/1/filter.php?c='+ category )
     fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c='+ category).then(function (response) {
         response.json().then(function (menu) {
             //saveMealID(menu);
             for (let j = 0; j < menu.meals.length; j++) {
                 //var menuTitle= document.createElement("h2");
-               // save meal id for all items in the search
-                mealID.push(menu.meals[j].idMeal);
-                console.log(mealID);
                 var menuTitle = document.createElement('button');
                 menuTitle.className = "menu-btn";
                 menuTitle.setAttribute("menu-id", menu.meals[j].idMeal)
@@ -311,8 +204,10 @@ function displayRecipeOptions(menu) {
             const ingredients = [];
 
             let recipeName = $('<h2>').addClass('title').text(data.meals[0].strMeal);
-            //let nextButton = $('<button>').attr('id', 'nxt-btn').text('Next Recipe');
             $('#recipe-name').append(recipeName);
+
+            console.log(recipeName);
+            //console.log(data.meals[0].strMeal);
 
             let recipeImg = $('<img>').attr('src', data.meals[0].strMealThumb);
             $('#recipe-img').append(recipeImg);
@@ -320,80 +215,42 @@ function displayRecipeOptions(menu) {
             console.log(recipeImg);
             
            //const ingredients = [];
-            // Get all ingredients from the object. Up to 20
-            let ingredientsList = $('<ul>').addClass('list');
-            var ingredientHeader = document.createElement("h2");
-            ingredientHeader.classList.add("title");
-            ingredientHeader.textContent = "Ingredients";
-            $('#recipe-ingredients').append(ingredientHeader);
-            for(let i=1; i<=20; i++) {
-                if(data.meals[0][`strIngredient${i}`]) {
-                ingredients.push(`${data.meals[0][`strIngredient${i}`]} - ${data.meals[0][`strMeasure${i}`]}`)
-                } else {
-                // Stop if no more ingredients
-                break;
-                }  
+    // Get all ingredients from the object. Up to 20
+    let ingredientsList = $('<ul>').addClass('list');
+    var ingredientHeader = document.createElement("h2");
+    ingredientHeader.classList.add("title");
+    ingredientHeader.textContent = "Ingredients";
+    $('#recipe-ingredients').append(ingredientHeader);
+     for(let i=1; i<=20; i++) {
+        if(data.meals[0][`strIngredient${i}`]) {
+        ingredients.push(`${data.meals[0][`strIngredient${i}`]} - ${data.meals[0][`strMeasure${i}`]}`)
+        } else {
+      // Stop if no more ingredients
+         break;
+        }  
         
-<<<<<<< HEAD
     }
-=======
-
-feature/mealDb-edamam-merge
->>>>>>> 447b378eac9ed397507de20e2f012e51a843be7d
     for(let x=0; x<ingredients.length; x++){
         let ingredientItem = $('<li>').addClass('list-item').text(ingredients[x]);
         ingredientsList.append(ingredientItem);
-
         
-            }
-            $('#recipe-ingredients').append(ingredientsList);
+    }
+    $('#recipe-ingredients').append(ingredientsList);
          
     
-            var recipeHeader = document.createElement("h2");
-            recipeHeader.classList.add("title");
-            recipeHeader.textContent = "Instructions";
-            $('#recipe-ingredients').append(recipeHeader);
+    var recipeHeader = document.createElement("h2");
+    recipeHeader.classList.add("title");
+    recipeHeader.textContent = "Instructions";
+    $('#recipe-ingredients').append(recipeHeader);
     
-            let instructions = $('<p>').addClass('instr').text(data.meals[0].strInstructions);   
-            $('#recipe-ingredients').append(instructions);
-    
-<<<<<<< HEAD
     let instructions = $('<p>').addClass('instr').text(data.meals[0].strInstructions);   
     $('#recipe-ingredients').append(instructions);
     }
  )};
-=======
->>>>>>> 447b378eac9ed397507de20e2f012e51a843be7d
 
-            var videoHeader = document.createElement("h2");
-            videoHeader.classList.add("title");
-            videoHeader.textContent = "Video Recipe";
-            var test = data.meals[0].strYoutube.slice(-11);
-            console.log(test);
-            $('#video').append(videoHeader);
-            var iframe = document.createElement('iframe');
-            iframe.src = "https://www.youtube.com/embed/"+ test;
-            console.log(iframe.src);
-            iframe.width = '420';
-            iframe.height = '315';
-            $('#video').append(iframe);
-  
- })
-}
 
 //$("menu-btn").on("click", buttonClickHandler);
-// function nextRecipe() {
-   
-//     //let value = $(this).text();
-//     console.log(mealID);
-//     recipeIndex = recipeIndex + 1;
-//     console.log(recipeIndex);
-//     var newmealID = mealID[recipeIndex];
-//     console.log(newmealID);
-//     displayRecipeOptions(newmealID);
 
-// };
-//$("#nxt-btn").on("click", nextRecipe);
 
 $("#edamam-btn").on("click", runEdamam);
 $("#mealdb-btn").on("click", getMealDB);
@@ -406,7 +263,6 @@ $('#select1 li').click(function() {
      $("#ingredients").empty();
      $("#recipe-header").empty();
      $("#recipe-ingredients").empty();
-     $("#video").empty();
      
      
       var value  = $(this).text();
