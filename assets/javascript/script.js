faf1d1025b05b86d266895c1b94e7a989e5ed17a
ciyer87 <chitra.iyer00@gmail.com>
2021-05-02 21:07:05 -0700

    added video link

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,5 @@
 let hitsIndex = 0;
-
+let recipeIndex = 0;
 let mealID = [];
 var listTitle = document.querySelector("#recipe-name");
 var meal_container = document.getElementById('#recipe-display');
@@ -197,12 +197,16 @@ var getMealDB = function (category) {
     //    console.log(data);
     //});
     //});
+    var mealID=[];
     console.log('https://www.themealdb.com/api/json/v1/1/filter.php?c='+ category )
     fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c='+ category).then(function (response) {
         response.json().then(function (menu) {
             //saveMealID(menu);
             for (let j = 0; j < menu.meals.length; j++) {
                 //var menuTitle= document.createElement("h2");
+               // save meal id for all items in the search
+                mealID.push(menu.meals[j].idMeal);
+                console.log(mealID);
                 var menuTitle = document.createElement('button');
                 menuTitle.className = "menu-btn";
                 menuTitle.setAttribute("menu-id", menu.meals[j].idMeal)
@@ -240,10 +244,8 @@ function displayRecipeOptions(menu) {
             const ingredients = [];
 
             let recipeName = $('<h2>').addClass('title').text(data.meals[0].strMeal);
+            //let nextButton = $('<button>').attr('id', 'nxt-btn').text('Next Recipe');
             $('#recipe-name').append(recipeName);
-
-            console.log(recipeName);
-            //console.log(data.meals[0].strMeal);
 
             let recipeImg = $('<img>').attr('src', data.meals[0].strMealThumb);
             $('#recipe-img').append(recipeImg);
@@ -251,42 +253,67 @@ function displayRecipeOptions(menu) {
             console.log(recipeImg);
             
            //const ingredients = [];
-    // Get all ingredients from the object. Up to 20
-    let ingredientsList = $('<ul>').addClass('list');
-    var ingredientHeader = document.createElement("h2");
-    ingredientHeader.classList.add("title");
-    ingredientHeader.textContent = "Ingredients";
-    $('#recipe-ingredients').append(ingredientHeader);
-     for(let i=1; i<=20; i++) {
-        if(data.meals[0][`strIngredient${i}`]) {
-        ingredients.push(`${data.meals[0][`strIngredient${i}`]} - ${data.meals[0][`strMeasure${i}`]}`)
-        } else {
-      // Stop if no more ingredients
-         break;
-        }  
+            // Get all ingredients from the object. Up to 20
+            let ingredientsList = $('<ul>').addClass('list');
+            var ingredientHeader = document.createElement("h2");
+            ingredientHeader.classList.add("title");
+            ingredientHeader.textContent = "Ingredients";
+            $('#recipe-ingredients').append(ingredientHeader);
+            for(let i=1; i<=20; i++) {
+                if(data.meals[0][`strIngredient${i}`]) {
+                ingredients.push(`${data.meals[0][`strIngredient${i}`]} - ${data.meals[0][`strMeasure${i}`]}`)
+                } else {
+                // Stop if no more ingredients
+                break;
+                }  
         
-    }
-    for(let x=0; x<ingredients.length; x++){
-        let ingredientItem = $('<li>').addClass('list-item').text(ingredients[x]);
-        ingredientsList.append(ingredientItem);
+            }
+            for(let x=0; x<ingredients.length; x++){
+                let ingredientItem = $('<li>').addClass('list-item').text(ingredients[x]);
+                ingredientsList.append(ingredientItem);
         
-    }
-    $('#recipe-ingredients').append(ingredientsList);
+            }
+            $('#recipe-ingredients').append(ingredientsList);
          
     
-    var recipeHeader = document.createElement("h2");
-    recipeHeader.classList.add("title");
-    recipeHeader.textContent = "Instructions";
-    $('#recipe-ingredients').append(recipeHeader);
+            var recipeHeader = document.createElement("h2");
+            recipeHeader.classList.add("title");
+            recipeHeader.textContent = "Instructions";
+            $('#recipe-ingredients').append(recipeHeader);
     
-    let instructions = $('<p>').addClass('instr').text(data.meals[0].strInstructions);   
-    $('#recipe-ingredients').append(instructions);
-    }
- )}
+            let instructions = $('<p>').addClass('instr').text(data.meals[0].strInstructions);   
+            $('#recipe-ingredients').append(instructions);
+    
 
+            var videoHeader = document.createElement("h2");
+            videoHeader.classList.add("title");
+            videoHeader.textContent = "Video Recipe";
+            var test = data.meals[0].strYoutube.slice(-11);
+            console.log(test);
+            $('#video').append(videoHeader);
+            var iframe = document.createElement('iframe');
+            iframe.src = "https://www.youtube.com/embed/"+ test;
+            console.log(iframe.src);
+            iframe.width = '420';
+            iframe.height = '315';
+            $('#video').append(iframe);
+  
+ })
+}
 
 //$("menu-btn").on("click", buttonClickHandler);
+// function nextRecipe() {
+   
+//     //let value = $(this).text();
+//     console.log(mealID);
+//     recipeIndex = recipeIndex + 1;
+//     console.log(recipeIndex);
+//     var newmealID = mealID[recipeIndex];
+//     console.log(newmealID);
+//     displayRecipeOptions(newmealID);
 
+// };
+//$("#nxt-btn").on("click", nextRecipe);
 
 $("#edamam-btn").on("click", runEdamam);
 $("#mealdb-btn").on("click", getMealDB);
@@ -299,6 +326,7 @@ $('#select1 li').click(function() {
      $("#ingredients").empty();
      $("#recipe-header").empty();
      $("#recipe-ingredients").empty();
+     $("#video").empty();
      
      
       var value  = $(this).text();
